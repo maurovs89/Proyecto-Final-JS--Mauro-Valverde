5e0d387c0320c199b3d857fa038b8b87e4cc74b0
maurovs89 <mauro.valverde.salcedo@gmail.com>
2024-04-25 12:43:49 -0300

    ejercicio 1 trabaj final terminado para entregar

--- main.js
+++ main.js
@@ -2,21 +2,23 @@
 let totalCompra = 0;
 let continuarComprando = true;
 
-// Solicitamos los productos y precios al usuario hasta que decida no comprar más
+// Le pido los productos al usuario, 
+// luego el precio, 
+// verifico q los precios sean mayor a cero
+// sumo los precios en totalCompra
+// y por ultimo preguntamos si desea seguir comprando
+
 while (continuarComprando) {
     let producto = prompt("Ingrese el nombre del producto:");
     let precio = parseFloat(prompt("Ingrese el precio del producto:"));
 
-    // Verificamos si se ingresó un precio válido
     if (precio > 0) {
-        // Sumamos el precio del producto al total de la compra
         totalCompra += precio;
     } else {
         alert("El precio ingresado no es válido. Por favor, ingrese un precio mayor a cero");
     }
 
-    // Preguntamos al usuario si desea seguir comprando
-    const respuesta = prompt("¿Desea ingresar otro producto? (Sí/No)").toLowerCase();
+    let respuesta = prompt("¿Desea ingresar otro producto? (Sí/No)").toLowerCase();
     if (respuesta === "sí" || respuesta === "si") {
         continuarComprando = true;
     } else {
@@ -25,7 +27,7 @@ while (continuarComprando) {
 
 }
 
-// Mostramos el total de la compra al usuario
+// Por último mostramos el total
 alert("El total de su compra es: " + totalCompra);
 
     
